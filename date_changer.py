62616a159d7b9e8f4a97ce46f6f02021d240f94e
Татьяна <syhoterina@gmail.com>
2025-01-19 18:44:40 +0300

    Create date_changer.py

---
+++ date_changer.py
@@ -0,0 +1,18 @@
+created_date = "Дата создания"
+print(created_date)
+created_date =input("Введите число: "+"Введите месяц "+"Введите год: ")
+#created_date =input("Введите число: ")
+#created_date =input("Введите месяц: ")
+#created_date =input("Введите год: ")
+issue_date = "Дата завершения"
+print(issue_date)
+issue_date =input("Введите число: "+"Введите месяц "+"Введите год: ")
+#issue_date =input("Введите число: ")
+#issue_date =input("Введите месяц: ")
+#issue_date =input("Введите год: ")
+print("12-02-2025")
+info = "12-02-2025"
+print(info[0:5])
+print("12 марта 2025")
+info = "12 марта 2025"
+print(info[0:8])
