5d62799da67240ed512fbf21a45a179cef1fd095
Татьяна <syhoterina@gmail.com>
2025-01-20 19:13:17 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,11 @@
+sernameu = input("Имя пользователя: ")
+print(f"Привет, {sernameu}")
+title = input("Заголовок заметки: ")
+content = input("Описание заметки: ")
+status = input("Статус заметки: ")
+created_date = input("Дата создания:\n дд-мм-гггг; ")
+info = "12-02-2025"
+print(info[0:5])
+issue_date = input("Дата истечения: \nдд-мм-гггг: ")
+info = "12-02-2025"
+print(info[0:5])
