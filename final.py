eecf255a5ee44c2d77b698bb6d9a56e08b24a3b7
Татьяна <syhoterina@gmail.com>
2025-01-29 19:52:11 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,14 @@
+names = ["Имя пользователя"]
+# print(names[0:1])
+content = ["Содержание заметки"]
+# print(content)
+status = ["Статус"]
+# print(status)
+creature = ["Дата создания"]
+# print(creature)
+ending = ["Дата изменения"]
+# print(ending)
+title = ["Заголовок 1", "Заголовок 2"]
+# print(title)
+list='Имя пользователя: \nСодержание заметки: \nСтатус:\nДата создания:\nДата изменения:\n[Заголовок 1: Заголовок 2:]'
+print(list)
