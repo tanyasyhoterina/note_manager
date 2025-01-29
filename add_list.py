e8a7d57f85ced519189a905df17195b951fe5d5d
Татьяна <syhoterina@gmail.com>
2025-01-29 17:39:41 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,27 @@
+username = "Имя пользователя"
+title = "Заговоловок заметки"
+content = "Описание заметки"
+status = "Статус заметки"
+created_date = "10-11-2024"
+issue_date = "12-11-2024"
+sername = input("Имя пользователя: ")
+print(f"Привет, {sername}")
+
+title1="1 Основные темы"
+tinle2="2 Персонажи"
+title3="3 Рекомендации для чтения"
+title = input("Заголовок заметки: ")
+title1=input("1 Основные темы: ")
+tinle2=input("2 Персонажи: ")
+title3=input("3 Рекомендации для чтения: ")
+
+content = input("Описание заметки: ")
+status = input("Статус заметки: ")
+created_date = input("Создано: дд-мм-гггг: ")
+print(created_date)
+info=("10-11-2024")
+print(info[:5])
+issue_date = input("Выполнить до: дд-мм-гггг: ")
+print(issue_date)
+info=("12-11-2024")
+print(info[:5])
